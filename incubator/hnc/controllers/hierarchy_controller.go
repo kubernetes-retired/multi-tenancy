526162a4bd36f2881240c624cb30dd158f204543
Adrian Ludwin <aludwin@google.com>
2019-09-19 14:17:38 -0400

    Idiomatic watches in the hierarchy controller
    Instead of directly updating any affected namespaces (ie old parents,
    children) when a namespace is updated, give the affected namespaces to
    the controller-runtime directly. This means that even if there's a
    problem saving the "original" namespace, the list of affected namespaces
    is stored by controller-runtime and they'll all eventually get resolved.
    This fixes issue #59.
    
    At the same time, I've fully removed the namespace reconciller and
    simply added a mapping function to the hierarchy reconciller to notice
    changes to namespaces.
    
    Tested: integ tests pass; demo works.

--- incubator/hnc/controllers/hierarchy_controller.go
+++ incubator/hnc/controllers/hierarchy_controller.go
@@ -26,6 +26,10 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	tenancy "github.com/kubernetes-sigs/multi-tenancy/incubator/hnc/api/v1alpha1"
 	"github.com/kubernetes-sigs/multi-tenancy/incubator/hnc/pkg/forest"
@@ -50,6 +54,11 @@ type HierarchyReconciler struct {
 	// This is probably wildly inefficient, and we can probably make better use of things like
 	// owner references to make this better. But for a PoC, it works just fine.
 	Types []NamespaceSyncer
+
+	// Affected is a channel of event.GenericEvent (see "Watching Channels" in
+	// https://book-v1.book.kubebuilder.io/beyond_basics/controller_watches.html) that is used to
+	// enqueue additional namespaces that need updating.
+	Affected chan event.GenericEvent
 }
 
 // NamespaceSyncer syncs various aspects of a namespace. The HierarchyReconciler both implements
@@ -61,17 +70,18 @@ type NamespaceSyncer interface {
 
 // +kubebuilder:rbac:groups=hnc.x-k8s.io,resources=hierarchies,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=hnc.x-k8s.io,resources=hierarchies/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch;update;patch
 
 // Reconcile simply calls SyncNamespace, which can also be called if a namespace is created or
 // deleted.
 func (r *HierarchyReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
-	nnm := req.NamespacedName
-	log := r.Log.WithValues("trigger", req.NamespacedName)
-	return ctrl.Result{}, r.SyncNamespace(ctx, log, nnm.Namespace)
+	ns := req.NamespacedName.Namespace
+	log := r.Log.WithValues("ns", ns)
+	return ctrl.Result{}, r.reconcile(ctx, log, ns)
 }
 
-func (r *HierarchyReconciler) SyncNamespace(ctx context.Context, log logr.Logger, nm string) error {
+func (r *HierarchyReconciler) reconcile(ctx context.Context, log logr.Logger, nm string) error {
 	update := true
 	missing := false
 
@@ -80,12 +90,12 @@ func (r *HierarchyReconciler) SyncNamespace(ctx context.Context, log logr.Logger
 	inst := &tenancy.Hierarchy{}
 	if err := r.Get(ctx, nnm, inst); err != nil {
 		if !errors.IsNotFound(err) {
-			log.Error(err, "Couldn't read singleton", "ns", nm)
+			log.Error(err, "Couldn't read singleton")
 			return err
 		}
 
 		// It doesn't exist - initialize it to a sane initial value.
-		log.V(1).Info("Missing singleton", "ns", nm)
+		log.V(1).Info("Missing singleton")
 		missing = true
 		inst.ObjectMeta.Name = tenancy.Singleton
 		inst.ObjectMeta.Namespace = nm
@@ -93,8 +103,10 @@ func (r *HierarchyReconciler) SyncNamespace(ctx context.Context, log logr.Logger
 
 	// If the hierarchy object exists but is being deleted, we won't update it when we're finished
 	// syncing (we should sync our internal data structure anyway just in case something's changed).
+	// I'm not sure if this is the right thing to do but the kubebuilder boilerplate included this
+	// case explicitly.
 	if !inst.GetDeletionTimestamp().IsZero() {
-		log.Info("Singleton is being deleted", "ns", nm)
+		log.Info("Singleton is being deleted")
 		update = false
 	}
 
@@ -105,26 +117,26 @@ func (r *HierarchyReconciler) SyncNamespace(ctx context.Context, log logr.Logger
 		nsnnm := types.NamespacedName{Name: nm}
 		if err := r.Get(ctx, nsnnm, ns); err != nil {
 			if !errors.IsNotFound(err) {
-				log.Error(err, "Couldn't read namespace", "ns", nm)
+				log.Error(err, "Couldn't read namespace")
 				return err
 			}
 
 			// The namespace is gone. Remove all traces of the namespace from  our data structures.
-			log.Info("Namespace was deleted", "ns", nm)
+			log.Info("Namespace was deleted")
 			return r.onDelete(ctx, log, nm)
 		}
 
 		// If the namespace is being deleted, we should still sync any changes that we might have missed
-		// (ie, maybe the parent used to be set?) but then don't update anything.
+		// (ie, maybe the parent used to be set?) but then don't update anything. I'm not sure if this
+		// is the right thing to do but the kubebuilder boilerplate included this case explicitly.
 		if !ns.GetDeletionTimestamp().IsZero() {
-			log.Info("Namespace is being deleted", "ns", nm)
+			log.Info("Namespace is being deleted")
 			update = false
 		}
 	}
 
-	// Sync the tree. If `ok` is false, that means that there are conditions on this namespace and we
-	// shouldn't continue to sync the objects.
-	if ok, err := r.updateTree(ctx, log, inst, update); !ok || err != nil {
+	// Sync the tree.
+	if err := r.updateTree(ctx, log, inst, update); err != nil {
 		return err
 	}
 
@@ -132,7 +144,7 @@ func (r *HierarchyReconciler) SyncNamespace(ctx context.Context, log logr.Logger
 		// Update all the objects in this namespace. We have to do this at least *after* the tree is
 		// updated, because if we don't, we could incorrectly think we've propagated the wrong objects
 		// from our ancestors, or are propagating the wrong objects to our descendants.
-		return r.updateObjects(ctx, log, inst.ObjectMeta.Namespace)
+		return r.updateObjects(ctx, log, nm)
 	}
 
 	return nil
@@ -140,12 +152,11 @@ func (r *HierarchyReconciler) SyncNamespace(ctx context.Context, log logr.Logger
 
 // updateTree syncs the Hierarchy singleton with the in-memory forest (writing back to the apiserver
 // if necessary and requested) and calls itself on any affected namespaces if the hierarchy has
-// changed. It returns false if there are conditions on this namespace, which indicates that any
-// changes from this namespace should not be further propagated.
+// changed.
 //
 // TODO: store the conditions in the in-memory forest so that object propagation can be disabled if
 // there's a problem on the namespace.
-func (r *HierarchyReconciler) updateTree(ctx context.Context, log logr.Logger, inst *tenancy.Hierarchy, update bool) (bool, error) {
+func (r *HierarchyReconciler) updateTree(ctx context.Context, log logr.Logger, inst *tenancy.Hierarchy, update bool) error {
 	// Update the in-memory data structures
 	orig := inst.DeepCopy()
 	affected := r.syncWithForest(ctx, log, inst)
@@ -154,32 +165,26 @@ func (r *HierarchyReconciler) updateTree(ctx context.Context, log logr.Logger, i
 	// if the caller wants us to update it (ie, if the namespace isn't being deleted).
 	if update && !reflect.DeepEqual(orig, inst) {
 		if inst.CreationTimestamp.IsZero() {
-			log.Info("Creating singleton on apiserver", "ns", inst.ObjectMeta.Namespace)
+			log.Info("Creating singleton on apiserver")
 			if err := r.Create(ctx, inst); err != nil {
 				log.Error(err, "while creating on apiserver")
-				return false, err
+				return err
 			}
 		} else {
-			log.Info("Updating singleton on apiserver", "ns", inst.ObjectMeta.Namespace)
+			log.Info("Updating singleton on apiserver")
 			if err := r.Update(ctx, inst); err != nil {
 				log.Error(err, "while updating apiserver")
-				return false, err
+				return err
 			}
 		}
 	}
 
-	// Stop updating if there are any conditions
-	if len(inst.Status.Conditions) > 0 {
-		log.Info("Early exit due to conditions")
-		return false, nil
-	}
-
 	// Update any other namespaces that we believe may have been affected
 	if err := r.updateAffected(ctx, log, affected); err != nil {
-		return false, err
+		return err
 	}
 
-	return true, nil
+	return nil
 }
 
 // updateObjects calls all type reconcillers in this namespace.
@@ -272,15 +277,29 @@ func (r *HierarchyReconciler) onDelete(ctx context.Context, log logr.Logger, nm 
 func (r *HierarchyReconciler) updateAffected(ctx context.Context, log logr.Logger, affected []string) error {
 	// TODO: parallelize updates
 	for _, nm := range affected {
-		if err := r.SyncNamespace(ctx, log, nm); err != nil {
-			return err
-		}
+		// The watch handler doesn't care about anything except the metadata.
+		inst := &tenancy.Hierarchy{}
+		inst.ObjectMeta.Name = tenancy.Singleton
+		inst.ObjectMeta.Namespace = nm
+		r.Affected <- event.GenericEvent{Meta: inst}
 	}
 	return nil
 }
 
 func (r *HierarchyReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// Maps namespaces to their singletons
+	nsMapFn := handler.ToRequestsFunc(
+		func(a handler.MapObject) []reconcile.Request {
+			return []reconcile.Request{
+				{NamespacedName: types.NamespacedName{
+					Name:      tenancy.Singleton,
+					Namespace: a.Meta.GetName(),
+				}},
+			}
+		})
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&tenancy.Hierarchy{}).
+		Watches(&source.Channel{Source: r.Affected}, &handler.EnqueueRequestForObject{}).
+		Watches(&source.Kind{Type: &corev1.Namespace{}}, &handler.EnqueueRequestsFromMapFunc{ToRequests: nsMapFn}).
 		Complete(r)
 }
