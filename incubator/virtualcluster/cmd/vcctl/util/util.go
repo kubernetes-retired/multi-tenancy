8d154a2a188cf57626883c65dafb7d8dda807844
chao zheng <charleszheng44@gmail.com>
2020-04-02 18:34:45 -0700

    1. use scheme.Scheme for vcctl 2. if kubelet-client-crt is empty or not set, forward the request to super master 2. `vcctl create` supports reading yaml file from url

--- incubator/virtualcluster/cmd/vcctl/util/util.go
+++ incubator/virtualcluster/cmd/vcctl/util/util.go
@@ -2,7 +2,8 @@ package util
 
 import (
 	"io/ioutil"
-	"os"
+	"net/http"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
@@ -11,20 +12,37 @@ import (
 // yamlToObj read yaml from yamlPath and deserialize to a runtime.Object
 // NOTE: make sure the target object type is added to scheme
 func YamlToObj(scheme *runtime.Scheme, yamlPath string) (runtime.Object, error) {
-	yamlFn, err := os.OpenFile(yamlPath, os.O_RDONLY, 0644)
+	yamlContent, err := getYamlContent(yamlPath)
 	if err != nil {
 		return nil, err
 	}
-	yamlContent, err := ioutil.ReadAll(yamlFn)
-	if err != nil {
-		return nil, err
-	}
-
 	decode := serializer.NewCodecFactory(scheme).UniversalDeserializer().Decode
-	obj, _, err := decode([]byte(yamlContent), nil, nil)
+	obj, _, err := decode(yamlContent, nil, nil)
 	if err != nil {
 		return nil, err
 	}
-
 	return obj, nil
+}
+
+// getYamlContent reads the yaml content from the `yamlPath`
+func getYamlContent(yamlPath string) ([]byte, error) {
+	if isURL(yamlPath) {
+		// read from an URL
+		resp, err := http.Get(yamlPath)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		yamlContent, err := ioutil.ReadAll(resp.Body)
+		return yamlContent, nil
+	}
+	// read from a file
+	yamlContent, err := ioutil.ReadFile(yamlPath)
+	return yamlContent, err
+}
+
+// isURL checks if `path` is an URL
+func isURL(path string) bool {
+	return strings.HasPrefix(path, "https://") || strings.HasPrefix(path, "http://")
 }
