c4af9568a1d87a95275593044a55ba6e494a3394
Chris Hein <me@chrishein.com>
2020-06-29 12:02:18 -0700

    adding ClusterNamespace to VC.Status
    Signed-off-by: Chris Hein <me@chrishein.com>

--- incubator/virtualcluster/pkg/apis/tenancy/v1alpha1/virtualcluster_types.go
+++ incubator/virtualcluster/pkg/apis/tenancy/v1alpha1/virtualcluster_types.go
@@ -58,6 +58,11 @@ type VirtualClusterStatus struct {
 	// cluster phase of the virtual cluster
 	Phase ClusterPhase `json:"phase"`
 
+	// ClusterNamespace defines the namespace where the control plane components
+	// are deployed into.
+	// +optional
+	ClusterNamespace string `json:"clusterNamespace,omitempty"`
+
 	// A human readable message indicating details about why the cluster is in
 	// this condition.
 	// +optional
