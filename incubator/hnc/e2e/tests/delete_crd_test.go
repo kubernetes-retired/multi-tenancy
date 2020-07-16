65dc872addbe3bd8ef1458721bbd35b7d0020bc5
Ginny <ginnyji@google.com>
2020-07-16 17:51:12 +0000

    Add e2e test for issue 328
    Add issue_test.go that test github issue #328. Add helper function
    runShouldContain() to run the command and check if the output contains
    the give slice of strings.
    
    Tested: make e2e-test

--- incubator/hnc/e2e/tests/delete_crd_test.go
+++ incubator/hnc/e2e/tests/delete_crd_test.go
@@ -76,6 +76,6 @@ var _ = Describe("When deleting CRDs", func() {
 		mustRun("kubectl delete crd hncconfigurations.hnc.x-k8s.io")
 		// Give HNC 10s to have the chance to fully delete everything (5s wasn't enough).
 		// Verify that the HNC CRDs are gone (if nothing's printed, then they are).
-		runShouldNotContain("hnc", "10s", "kubectl get crd")
+		runShouldNotContain("hnc", 10, "kubectl get crd")
 	})
 })
