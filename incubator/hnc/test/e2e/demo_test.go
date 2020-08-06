515185f7ed912b46d39e8959e387f99df3685991
Ginny <ginnyji@google.com>
2020-08-06 15:23:54 +0000

    Remove the focused test
    Forgot to remove the focused test from last PR
    
    Tested: make e2e-test

--- incubator/hnc/test/e2e/demo_test.go
+++ incubator/hnc/test/e2e/demo_test.go
@@ -93,7 +93,7 @@ var _ = Describe("Demo", func() {
 		RunShouldNotContain("my-creds", 2, "kubectl -n", nsService1, "get secrets")
 	})
 
-	FIt("Should intergrate hierarchical network policy", func(){
+	It("Should intergrate hierarchical network policy", func(){
 		MustRun("kubectl create ns", nsOrg)
 		MustRun("kubectl hns create", nsTeamA, "-n", nsOrg)
 		MustRun("kubectl hns create", nsTeamB, "-n", nsOrg)
